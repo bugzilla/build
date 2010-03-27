18a4c87d67ae7a26cc7b2762bcba230fe25d41de
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-03-27 15:23:44 -0700

    The scripts used to build Bugzilla releases and diff files. These have been around for years, evolving over time, but this is the first time they're in version control.

---
+++ buildtarball.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+echo "This script will build a Bugzilla tarball based on a given CVS tag."
+if [ ! "X$1" = "X" ] ; then
+  BZTAG=$1
+  echo "Using CVS tag from command line: $BZTAG"
+else
+  echo -n "Enter the CVS tag to use: "
+  read BZTAG
+  if [ "X$BZTAG" = "X" ] ; then
+    echo "You entered an empty string - aborting."
+    exit
+  fi
+fi
+if [ ! "X$2" = "X" ] ; then
+  BZDIR=$2
+  echo "Using unpack directory name from command line: $BZDIR"
+else
+  echo "Enter the name of the folder that should be created when the end user"
+  echo -n "unpacks the tarball: "
+  read BZDIR
+  if [ "X$BZDIR" = "X" ] ; then
+    echo "You entered an empty string - aborting."
+    exit
+  fi
+fi
+
+CMD="cvs -d :pserver:anonymous@cvs-mirror.mozilla.org:/cvsroot co -d $BZDIR -r $BZTAG Bugzilla"
+echo $CMD
+$CMD
+if [ ! -e $BZDIR ] ; then
+  echo "$0: CVS checkout failed"
+  exit
+fi
+
+echo "cd $BZDIR"
+cd $BZDIR
+echo "cvs -q up -dP"
+cvs -q up -dP
+if [ ! -e docs/pdf ] ; then
+  echo "Building docs..."
+  perl -w docs/makedocs.pl --with-pdf
+fi
+echo "cd .."
+cd ..
+echo "tar cfz ${BZDIR}.tar.gz $BZDIR"
+tar cfz ${BZDIR}.tar.gz $BZDIR
+echo "Done."
---
+++ makediffs.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+
+use strict;
+sub dosys {
+  print "@_\n";
+  system @_;
+}
+sub do_diff {
+    my ($major, $minor, $micro) = @_;
+
+    my $latest_ver = "$major.$minor.$micro";
+    my $latest = "bugzilla-$latest_ver";
+    my $base = "bugzilla-$major.$minor";
+
+    my @prior_micro = map { ".$_" } (1..$micro-1);
+    foreach my $this ('', @prior_micro) {
+        my $diffing = "$base$this";
+        dosys("diff -urN --exclude=CVS --exclude=*.pdf $diffing $latest"
+              . " > $diffing-to-$latest_ver.diff");
+        dosys("gzip $diffing-to-$latest_ver.diff");
+        dosys("diff -urN --exclude=CVS --exclude=docs $diffing $latest"
+              . " > $diffing-to-$latest_ver-nodocs.diff");
+        dosys("gzip $diffing-to-$latest_ver-nodocs.diff");
+    }
+}
+
+my @tarballs = glob('./bugzilla-*.tar.gz');
+
+foreach my $tarball (@tarballs) {
+    if ($tarball =~ /bugzilla-(\d)\.(\d+)\.(\d+).tar.gz/) {
+        my ($major, $minor, $micro) = ($1, $2, $3);
+        next if $minor % 2; # Don't do odd-numbered releases
+        do_diff($major, $minor, $micro);
+    }
+}
